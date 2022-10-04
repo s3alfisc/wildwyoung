55d000b996d5a560ec9a638a6068805efa902cfe
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-10-04 18:36:43 +0200

    first draft of wyoung procedure

---
+++ R/wildwyoung.R
@@ -0,0 +1,260 @@
+# models <- fit
+# param <- "treatment"
+# B <- 999
+# nsims <- 1000
+# R <- NULL
+# r <- 0
+# p_val_type <- "two-tailed"
+# weights_type = "rademacher"
+# seed = NULL
+# engine = "R"
+# nthreads = 1
+
+
+wyoung <- function(models, param, B, nsims, R = NULL, r = 0, p_val_type = "two-tailed", weights_type = "rademacher", seed = NULL, engine = "R", nthreads = 1, ...){
+
+  #' Westfall-Young multiple hypotheses adjusted p-values
+  #'
+  #' Function implements the Westfall-Young multiple hypthesis correction procedure for objects of type fixest_multi (fixest_multi are objects created by `fixest::feols()` that use `feols()` multiple-estimation interface).
+  #' Currently, the command is restricted to two-sided hypotheses and oneway clustered standard errors. For the wild cluster bootstrap,
+  #' the null is always imposed.
+  #' @param models An object of type fixest_multi or a list of objects of type fixest
+  #' @param param The regression parameter to be tested
+  #' @param R Hypothesis Vector giving linear combinations of coefficients. Must be either NULL or a vector of the same length as `param`. If NULL, a vector of ones of length param.
+  #' @param r A numeric. Shifts the null hypothesis
+  #'        H0: param = r vs H1: param != r
+  #' @param B The number of bootstrap iterations
+  #' @param nsims The number of Monte Carlo Iterations
+  #' @param p_val_type Character vector of length 1. Type of hypothesis test
+  #'        By default "two-tailed". Other options include "equal-tailed", ">" and "<".
+  #' @param weights_type character or function. The character string specifies the type
+  #'                     of boostrap to use: One of "rademacher", "mammen", "norm"
+  #'                     and "webb". Alternatively, type can be a function(n) for drawing
+  #'                     wild bootstrap factors. "rademacher" by default.
+  #'                     For the Rademacher distribution, if the number of replications B exceeds
+  #'                     the number of possible draw ombinations, 2^(#number of clusters), then `boottest()`
+  #'                     will use each possible combination once (enumeration).
+  #' @param seed Integer. Sets the random seed. NULL by default.
+  #' @param engine Should the wild cluster bootstrap run via fwildclusterboot's R implementation or via WildBootTests.jl? 'R' by default. The other option is 'WildBootTests.jl'.
+  #' @param nthreads Integer. The number of threads to use.
+  #' @param ... additional function values passed to the bootstrap function.
+
+  #' @importFrom fwildclusterboot boottest
+  #' @importFrom fixest coeftable
+  #' @importFrom dreamerr check_arg
+  #' @importFrom stats terms formula
+  #' @export
+  #'
+  #' @examples
+  #'
+  #' library(fixest)
+  #' library(wildwyoung)
+  #'
+  #' set.seed(12345)
+  #'
+  #' N <- 1000
+  #' X1 <- rnorm(N)
+  #' Y1 <- 1 + 1 * X1 + rnorm(N)
+  #' Y2 <- 1 + 0.01 * X1 + rnorm(N)
+  #' Y3 <- 1 + 0.01 * X1 + rnorm(N)
+  #' Y4 <- 1 + 0.01 * X1 + rnorm(N)
+  #'
+  #' B <- 999
+  #' # intra-cluster correlation of 0 for all clusters
+  #' cluster <- rep(1:50, N / 50)
+  #'
+  #' data <- data.frame(Y1 = Y1,
+  #'                    Y2 = Y2,
+  #'                    Y3 = Y3,
+  #'                    Y4 = Y4,
+  #'                    X1 = X1,
+  #'                    cluster = cluster)
+  #'
+  #' res <- feols(c(Y1, Y2, Y3) ~ X1, data = data, cluster = ~ cluster)
+  #' res_wyoung <- wyoung(models = res, param = "X1", B = B)
+  #' summary(res_wyoung)
+  #'
+  #' @references
+  #' Clarke, Romano & Wolf (2019), STATA Journal. IZA working paper: https://ftp.iza.org/dp12845.pdf
+
+
+  check_arg(param, "character vector | character scalar | formula")
+  check_arg(R, "NULL | numeric vector")
+  check_arg(r, "NULL | numeric scalar")
+  check_arg(p_val_type, "charin(two_sided, >, <)")
+  check_arg(B, "integer scalar GT{99}")
+  check_arg(nsims, "integer scalar GT{99}")
+  check_arg(seed, "integer scalar | NULL")
+  check_arg(engine, "charin(R, R-lean, WildBootTests.jl)")
+  check_arg(nthreads, "scalar integer")
+
+  if (inherits(param, "formula")) {
+    param <- attr(terms(param), "term.labels")
+  }
+
+  # Check if 'models' is of type fixest_multi
+  if(!inherits(models, "fixest_multi")){
+  } else if(inherits(models, "list")){
+    fixest_list <- mean(sapply(models, class) == "fixest") == 1L
+    if(!fixest_list){
+      stop("The object models needs to be either of type 'fixest_multi' or a list of objects of type 'fixest'.")
+    }
+  }
+
+
+  call <- models[[1]]$call
+  S <- length(models)
+
+  # define a function to get statistics from fixest_multi object
+  get_stats_fixest <- function(x, stat){
+    res <- fixest::coeftable(models[[x]])[which(rownames(fixest::coeftable(models[[x]])) == param), stat]
+    res
+  }
+
+  # and get coefs, t-stats and ses
+  # no absolute values for coefs, ses
+  # coefs <- unlist(lapply(1:S, function(x) get_stats_fixest(x, stat = "Estimate")))
+  # ses <- unlist(lapply(1:S, function(x) get_stats_fixest(x, stat = "Std. Error")))
+  # # absolute value for t-stats
+  # t_stats <- abs(unlist(lapply(1:S, function(x) get_stats_fixest(x, stat = "t value"))))
+
+  pvals <- abs(unlist(lapply(1:S, function(x) get_stats_fixest(x, stat = "Pr(>|t|)"))))
+
+  # get bootstrap p-values for WY procedure
+
+  get_pvals <- function(x, ssc){
+    #' @param x A tstatistic
+    #' @param ssc A small sample correction
+    #' @noRd
+    pvals <- 2 * min(pt(x, ssc), 1 - pt(x, ssc))
+  }
+  get_pvals <- Vectorize(get_pvals, vectorize.args = "x")
+
+  res <-
+    lapply(seq_along(models),
+           function(x){
+
+             clustid <- models[[x]]$call$cluster
+             N <- models[[x]]$nobs
+             k <- models[[x]]$nparams
+
+             if(!is.null(clustid)){
+
+               #G <- ...
+
+               t_boot <-
+                boottest(
+                     models[[x]],
+                     param = param,
+                     B = B,
+                     R = R,
+                     r = r,
+                     engine = engine,
+                     p_val_type = p_val_type,
+                     type = weights_type,
+                     clustid = formula(clustid)
+                  )$t_boot
+               pvals <- get_pvals(x = t_boot, ssc = N-k-1)
+               pvals
+             } else {
+               t_boot <-
+                   boottest(
+                     models[[x]],
+                     param = param,
+                     B = B,
+                     R = R,
+                     r = r,
+                     engine = engine,
+                     p_val_type = p_val_type,
+                     type = weights_type
+                   )$t_boot
+               pvals <- get_pvals(x = t_boot, ssc = N-k-1)
+               pvals
+             }
+           })
+
+  boot_pvals <- Reduce("cbind", res)
+
+  # start the westfall-young procedure
+
+  # order the p-values, small to large
+  pvals_order <- order(pvals)
+  pvals <- pvals[pvals_order]
+  # sort the bootstrap pvals
+  boot_pvals <- boot_pvals[,pvals_order]
+
+  boot_qvals <- matrix(NA, B, S)
+  for(i in 1:B){
+
+    boot_qvals[i,S] <- boot_pvals[i, S]
+    for(j in (S-1):1){
+      boot_qvals[i,j] <- min(boot_qvals[i,j+1], boot_pvals[i,j])
+    }
+
+  }
+
+  ps <- rep(NA,S)
+  for(s in 1:S){
+    ps[s] <- mean(boot_qvals[,s] < pvals[s])
+  }
+  ps <- colMeans((boot_qvals < pvals))
+
+  # enforce monotoniciy
+  pvalue_wy <- rep(NA, S)
+  pvalue_wy[1] <- ps[1]
+  for(s in 2:S){
+    pvalue_wy[s] <- max(pvalue_wy[s-1], ps[s])
+  }
+
+  pvalue_wy
+
+
+  # summarize all results
+  models_info <-
+    lapply(1:S, function(x){
+      tmp <- coeftable(models[[x]])
+      tmp1 <- tmp[which(rownames(tmp) == param),]
+      suppressWarnings(tmp1$depvar <- as.character(models[[x]]$fml[[2]]))
+      tmp1$model <- paste("Model", x)
+      tmp1
+    })
+
+  models_info <- Reduce(rbind, models_info)
+
+  # some reordering
+  models_info <- models_info[, c(6,5, 1:4)]
+  models_info <- as.data.frame(models_info)
+  # attributes(models_info)$row.names <- NULL
+  rownames(models_info) <- NULL
+  models_info[, "RW Pr(>|t|)"] <- pvalue_wy
+
+  res <- list(
+    call = call,
+    models_info = models_info,
+    coefs = coefs,
+    # ses = ses,
+    t_stats = t_stats,
+    boot_coefs = boot_coefs,
+    boot_ses = boot_ses,
+    # boot_t_stats = boot_t_stats,
+    pval = pvalue_wy
+  )
+
+  # create class of type wyoung
+  class(res) <- "wyoung"
+
+  invisible(res)
+
+
+}
+
+summary.wyoung <- function(object, digits, ...){
+  #' Summary method for objects of type wyoung
+  #' @param object An object of type wyoung
+  #' @param digits Rounding of digits
+  #' @param ... misc. function arguments
+  #' @export
+  as.data.frame(object$models_info)
+}
+
+
